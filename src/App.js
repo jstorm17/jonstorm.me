3783dbe68323a65dbf2987920646de16507cc97f
Jon Storm <9112768+iamjonstorm@users.noreply.github.com>
2019-02-25 18:09:53 -0600

    New Personal Site

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {me, twitter, github, linkedin, nodejs, react, serverless} from './images';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hi <span role="img" aria-label="wave">👋</span> I'm Jon Storm</p>
+        <img className="me" src={me} alt="jon_storm" />
+        <p>I live in Austin, TX</p>
+        <p>I studied <span style={{fontWeight: '400'}}>Computer Science</span> and <span style={{fontWeight: '400'}}>Business Management</span> at <a href="http://carthage.edu" style={{'color': '#da3f2b', textDecoration: 'none', fontWeight: '400'}}>Carthage College</a></p>
+        <p>I'm a full stack software engineer at <a href="http://uship.com" style={{color: '#2c83bb', textDecoration: 'none', fontWeight: '400'}}>uShip</a></p>
+        <p>I'm a semi-professional beach volleyball player at <a href="http://avp.com" style={{color: '#fcd333', textDecoration: 'none', fontWeight: '400'}}>AVP</a></p>
+        <div className="preferredStack">
+          <a href="https://nodejs.org/en/"><img src={nodejs} alt="nodejs"/></a>
+          <a href="https://serverless.com/"><img src={serverless} alt="twitter"/></a>
+          <a href="https://reactjs.org/"><img src={react} alt="twitter"/></a>
+        </div>
+        <div className="links">
+          <a href="https://twitter.com/iamjonstorm"><img src={twitter} alt="twitter"/></a>
+          <a href="https://github.com/iamjonstorm"><img src={github} alt="github"/></a>
+          <a href="http://www.linkedin.com/in/jon-storm"><img src={linkedin} alt="linkedin"/></a>
+        </div>
       </div>
     );
   }
---
+++ src/images/index.js
@@ -0,0 +1,8 @@
+export {default as github} from './github.png';
+export {default as linkedin} from './linkedin.png';
+export {default as me} from './me.jpeg';
+export {default as nodejs} from './nodejs.png';
+export {default as react} from './react.png';
+export {default as serverless} from './serverless.png';
+export {default as twitter} from './twitter.png';
+
